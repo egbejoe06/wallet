852f3475364c992d72509bffe696bb28b61c5644
Egbeleke Jonathan <bunkumijoe@gmail.com>
2024-03-01 05:09:57 -0800

    commit

--- src/main.js
+++ src/main.js
@@ -4,11 +4,5 @@ import App from './App.vue'
 import router from './router'
 import { createPinia } from 'pinia'
 
-// Polyfill for global object
-if (typeof global === "undefined") {
-    window.global = window;
-}
-
-
 const app = createApp(App);
 app.use(router).use(createPinia()).mount('#app');
